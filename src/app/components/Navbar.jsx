383a8ad3acefd4318fafefcfccdd5bfee7f44023
king6991 <mozieking6@gmail.com>
2025-03-10 17:03:24 -0700

    king react practise

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import link from 'next/link';
+
+export default function Navbar() {
+    <nav>
+        <ul className='flex items-center justify-center bg-slate-800,/45 text-gray-100 py-1 px-16 text-2xl shadow-2xl space-x-6'>
+            <li>
+                <link href={'/'}>Home</link>
+            </li>
+                    
+            <li>
+                <link href={'/about'}>About</link>  
+            </li>
+            <li>
+                <link href={'/contact'}>Contact</link>
+            </li>
+
+</ul>
+    </nav>
+}
